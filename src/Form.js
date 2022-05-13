bd0304b814074c7a4a077e89bc445175ef1c183c
Peter Flanders <pfflanders@gmail.com>
2022-05-13 03:17:39 -0700

    added some fields to work on centering

--- src/Form.js
+++ src/Form.js
@@ -3,14 +3,18 @@ import React, { useState } from "react";
 class Form extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
+        this.state = { value: '', quality: 0, entertainment: 0 };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ value: event.target.value });
+        this.setState({ 
+            value: event.target.value,
+            quality: event.target.quality,
+            entertainment: event.target.entertainment
+        });
     }
 
     handleSubmit(event) {
@@ -21,10 +25,30 @@ class Form extends React.Component {
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
+                <div class="auto">
+                    <label>
+                        Movie Title:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                </div>
+                <div class="col-50">
+                    <label>
+                            Date Viewed:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                </div>
+                <div class="col-75">
+                    <label>
+                            Entertainment Rating:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                </div>
+                <div class="col-25">
+                    <label>
+                            Quality Rating:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                </div>
                 <input type="submit" value="Submit" />
             </form>
         );
